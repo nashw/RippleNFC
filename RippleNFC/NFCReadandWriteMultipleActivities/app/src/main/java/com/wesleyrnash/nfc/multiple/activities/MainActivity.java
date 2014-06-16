bcd9ebf8b90967c642af59157f1370471616cdf1
lwcron <lucaswcron@gmail.com>
2014-06-16 15:33:02 -0400

    Cleaner version, add read and write classes

---
+++ RippleNFC/NFCReadandWriteMultipleActivities/app/src/main/java/com/wesleyrnash/nfc/multiple/activities/MainActivity.java
@@ -0,0 +1,112 @@
+package com.wesleyrnash.nfc.multiple.activities;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.IOException;
+
+public class MainActivity extends ActionBarActivity {
+
+    Button readButton;
+    Button writeButton;
+    TextView testTextView;
+    Context ctx;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ctx = this;
+
+        testTextView = (TextView) findViewById(R.id.testTextView);
+
+        readButton = (Button) findViewById(R.id.readButton);
+        writeButton = (Button) findViewById(R.id.writeButton);
+
+//        readButton.setOnClickListener(new View.OnClickListener()
+//        {
+//            @Override
+//            public void onClick(View view) {
+//                //try {
+//                    Intent intent = new Intent(ctx, ReadFromTagActivity.class);
+//                    startActivity(intent);
+//                //} catch (IOException e){
+//                    //e.printStackTrace();
+//                //}
+//            }
+//        });
+
+        writeButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                testTextView.setText("It worked!");
+            }
+        });
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+//    public void startReadActivity(View view){
+//        Intent intent = new Intent(this, ReadFromTagActivity.class);
+//        startActivity(intent);
+//    }
+//
+//    public void testText(View view){
+//        testTextView.setText("It worked!");
+//    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+    }
+
+}
